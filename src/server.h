20bb394a26cd7db79ffec95a50e7c2527daf2a7d
joinhack <joinhack@gmail.com>
2013-03-22 01:04:40 +0800

    add server.h

---
+++ src/server.h
@@ -0,0 +1,27 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "log.h"
+#include "jmalloc.h"
+#include "code.h"
+#include "cevent.h"
+#include "cnet.h"
+#include "cio.h"
+#include "cthread.h"
+#include "network.h"
+#include "obj.h"
+
+typedef struct {
+	int in_fd;
+	int un_fd;
+	int logfd;
+	pid_t pid;
+	uint32_t connections;
+	long last_info_time;
+	cevents *evts;
+	cthr_pool *thr_pool;
+} server;
+
+
+
+#endif /**end define server*/
